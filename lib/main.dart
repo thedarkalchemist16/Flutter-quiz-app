9c5a87023a2198230e3721706e649d1bb7d7933f
thedarkalchemist16 <shivangj.1601@gmail.com>
2022-07-27 15:54:39 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class MyFirstApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyFirstApp> {
+  var questionIndex = 0;
+  void answers() {
+    setState(() {
+      questionIndex += 1;
+    });
+    print('ANSWER SELECTED');
+  }
+
+  Widget build(BuildContext context) {
+    var questions = [
+      'What is your name?',
+      'What is your favorite colour',
+      'What is your favorite animal',
+      'What is your favorite city'
+    ];
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(title: Text('FIRST APP')),
+      body: Column(
+        children: [
+          Text(questions[questionIndex]),
+          ElevatedButton(
+            onPressed: answers,
+            child: Text("Answer 1"),
+          ),
+          ElevatedButton(
+            onPressed: answers,
+            child: Text("Answer 2"),
+          ),
+          ElevatedButton(
+            onPressed: answers,
+            child: Text("Answer 3"),
+          ),
+        ],
+      ),
+    ));
+  }
+}
+
+void main() {
+  runApp(MyFirstApp());
+}
