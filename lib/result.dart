d0b544aef46eafa8b045fe2c12819757a1bdb18a
thedarkalchemist16 <f20202449@hyderabad.bits-pilani.ac.in>
2022-08-17 01:33:36 +0530

    11th commit

--- lib/result.dart
+++ lib/result.dart
@@ -27,6 +27,7 @@ class Result extends StatelessWidget {
         child: Text(
       ResultPhrase,
       style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+      textAlign: TextAlign.center,
     ));
   }
 }
